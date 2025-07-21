afde4101fa585514622c721fa2674814eda1cadb
szymek1 <bogusszymon2000@gmail.com>
2025-07-21 17:45:38 +0200

    Prepared PWM module and added extra PWM parameters

--- src/hdl/pwm.v
+++ src/hdl/pwm.v
@@ -23,7 +23,35 @@
 
 
 module pmw (
-    
+    input                               sysclk,
+    input  wire                         i_enb,
+    input  wire [`BRIGHTNESS_WIDTH-1:0] i_d,   // duty cycle
+    output reg                          o_pmw,
+    output reg  [`BRIGHTNESS_WIDTH-1:0] o_cnt, // pmw register direct access
 
 );
+
+    localparam                  br_min = (2**`BRIGHTNESS_WIDTH - 1) * `LED_MIN_BRIGHTNESS / 100;
+    localparam                  br_max = (2**`BRIGHTNESS_WIDTH - 1) * `LED_MAX_BRIGHTNESS / 100;
+
+    reg [`BRIGHTNESS_WIDTH-1:0] r_d;
+
+    always @(posedge sysclk) begin
+        if (!enable) begin
+            o_cnt <= 2**`BRIGHTNESS_WIDTH - 1;
+            o_pmw <= 1'b0;
+        end else begin
+            // counter will overflow back to 0
+            o_cnt <= o_cnt + 1'b1;
+            o_pmw <= ((o_cnt + 1'b1) >= r_d) ? 1'b0 : 1'b1;
+        end
+
+        if (o_cnt == (2**`BRIGHTNESS_WIDTH - 1)) begin
+            if (i_d < `LED_MIN_BRIGHTNESS)      r_d <= br_min;
+            else if (i_d < `LED_MIN_BRIGHTNESS) r_d <= br_max;
+            else                                r_d <= i_d;
+        end
+
+    end
+
 endmodule
--- src/include/params.vh
+++ src/include/params.vh
@@ -2,8 +2,10 @@
 `define PARAMS_V
 
 // PWM (LED control)
-`define LED_MIN_BRIGHTNESS 5  // minimal level of brightness in %
-`define LED_MAX_BRIGHTNESS 90 // maximal level of brightness in %
+`define BRIGHTNESS_WIDTH   7  // brightness level can vary: 0%-100%; 7-bit number required to store the input value
+`define LED_MIN_BRIGHTNESS 5  // minimal level of brightness in %- min. duty cycle
+`define LED_MAX_BRIGHTNESS 90 // maximal level of brightness in %- max. duty cycle
+`define 
 
 
 `endif // PARAMS_V
