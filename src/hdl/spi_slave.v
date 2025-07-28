920c59567aa4f7e22f3e6926c53793d7c3f71dd5
szymek1 <bogusszymon2000@gmail.com>
2025-07-28 18:26:55 +0200

    Extended clokc details inside the description for SPI slave

--- src/hdl/spi_slave.v
+++ src/hdl/spi_slave.v
@@ -11,6 +11,9 @@
 // Description: SPI Slave module. This module decodes incomming message to either:
 //              specify which LED to light up with a certain % of brightness or
 //              to send back information about brightness level of the LED.
+//              Module configured for SPI Mode 0.
+//              It assumes an internal 125MHz clock and an external provided via
+//              sclk input 26MHz one.
 //
 // Dependencies: params.vh
 //
