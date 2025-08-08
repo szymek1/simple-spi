8bc8ff20e7d133c75011e58046a92fe871b2154c
szymek1 <bogusszymon2000@gmail.com>
2025-08-08 10:41:19 +0200

    Defined all inputs/outputs for spi slave tesbench

--- src/sim/spi_slave_tb.v
+++ src/sim/spi_slave_tb.v
@@ -20,3 +20,45 @@
 //////////////////////////////////////////////////////////////////////////////////
 `timescale 1ns/1ps
 `include "../include/params.vh"
+
+
+module spi_slave_tb (
+
+);
+
+    // Inputs: master&slave
+    reg                             clk;
+    
+    // Inputs: master
+    reg                             tx_enb;
+    reg  [`MASTER_FRAME_WIDTH-1:0]  i_frame;
+    wire                            miso; // output for slave
+
+    // Outputs: master -> inputs: slave
+    wire                            cs;
+    wire                            sclk;
+    wire                            mosi;
+    wire                            o_frame;
+
+    // Inputs: slave
+    reg                             slv_tx_enb;
+    reg  [`MASTER_FRAME_WIDTH-1:0]  i_slv_frame;
+
+    // Outputs: slave (everything except of miso)
+    wire [`CMD_BITS-1:0]            o_cmd;
+    wire [`ADDR_BITS-1:0]           o_addr;
+    wire [`PAYLOAD_BITS-1:0]        o_payload;
+
+    // Utils: used to instantiate master i_frame
+    reg  [`CMD_BITS-1:0]            mock_master_cmd_bits;
+    reg  [`ADDR_BITS-1:0]           mock_master_addr_bits;
+    reg  [`PAYLOAD_BITS-1:0]        mock_master_payload_bits;
+    // used for slave i_slv_frame
+    reg  [`CMD_BITS-1:0]            mock_slave_cmd_bits;
+    reg  [`ADDR_BITS-1:0]           mock_slave_addr_bits;
+    reg  [`PAYLOAD_BITS-1:0]        mock_slave_payload_bits;
+
+
+
+
+endmodule
